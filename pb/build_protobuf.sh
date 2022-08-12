748020f6b99333268eaa8c0440d72d46c3236544
unknown <57207701+oylsister@users.noreply.github.com>
2022-08-12 08:22:53 +0700

    upload all necessary file

--- pb/build_protobuf.sh
+++ pb/build_protobuf.sh
@@ -17,7 +17,7 @@ autoreconf -f -i -Wall,no-obsolete
 rm -rf autom4te.cache config.h.in~
 
 # Make sure to compile for 32bit with old ABI for std::string compatibility
-./configure --prefix=/home/alliedmodders/sourcemod/extensions/sm-ext-voice/pb --build=i686-pc-linux-gnu "CFLAGS=-m32 -D_GLIBCXX_USE_CXX11_ABI=0 -std=c++14" "CXXFLAGS=-m32 -D_GLIBCXX_USE_CXX11_ABI=0 -std=c++14" "LDFLAGS=-m32 -D_GLIBCXX_USE_CXX11_ABI=0 -std=c++14" --disable-shared --enable-static
+./configure --prefix=/root/alliedmodders/sourcemod/extensions/sm-ext-voice/pb --build=i686-pc-linux-gnu "CFLAGS=-m32 -D_GLIBCXX_USE_CXX11_ABI=0 -std=c++14" "CXXFLAGS=-m32 -D_GLIBCXX_USE_CXX11_ABI=0 -std=c++14" "LDFLAGS=-m32 -D_GLIBCXX_USE_CXX11_ABI=0 -std=c++14" --disable-shared --enable-static
 make -j 8
 make install
 
